c886f55015d2f26b51c7ca133f5b9713e2b2ac42
Neet <75447256+Chhez@users.noreply.github.com>
2024-07-11 10:08:12 -0700

    Created landing page
    - Used IntersectionObserver API
    - Added animations

--- Website/Frontend/src/components/Navbar.jsx
+++ Website/Frontend/src/components/Navbar.jsx
@@ -5,79 +5,70 @@ import cart_empty from '../assets/cart_empty.png';
 import logo from '../assets/logo.avif';
 import styles from './navbar.module.scss';
 
+// This component is the top navigation bar, and contains links to different pages
+// likely will not need to change for future unless needing more pages etc.
+
 function Navbar() {
     const learnElement = <span className={styles.navText}>Learn</span>;
     const impactElement = <span className={styles.navText}>Out Impact</span>;
     const aboutElement = <span className={styles.navText}>About Us</span>;
 
     return (<>
-        <Nbar variant="light" className="navbar-light bg-light" expand="lg">
-           <Container fluid className="ms-5 my-3">
-                <Nbar.Brand className="mx-4 px-0"> 
-                    <Link to="/"><img src={logo} width="120vh" height="52vh" className="d-inline-block align-top" alt="Logo" /></Link>
-                </Nbar.Brand>
-                <Nbar.Toggle aria-controls="navbarScroll" />
-                <Nbar.Collapse>
-                    <Nav variant="primary" className="mx-3" navbarScroll>
-                        <Nav.Link>
-                            <Link to="/" className={styles.navText}>Home</Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to="/shop" className={styles.navText}>Shop</Link>
-                        </Nav.Link>
-                        <NavDropdown title={learnElement}>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/eco-friendly-brands" className={styles.navTextNoHover}>Eco-Friendly Brands</Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/sustainability-activities" className={styles.navTextNoHover}>Sustainability Activities</Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/blog" className={styles.navTextNoHover}>Our Blog</Link>
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title={impactElement}>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/partners" className={styles.navTextNoHover}>Partners</Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/philanthropy" className={styles.navTextNoHover}>Philanthropy</Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/production" className={styles.navTextNoHover}>Production</Link>
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <NavDropdown title={aboutElement}>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/meet-the-team" className={styles.navTextNoHover}>Meet The Team</Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/contact-us" className={styles.navTextNoHover}>Contact Us</Link>
-                            </NavDropdown.Item>
-                            <NavDropdown.Item style={{backgroundColor: "white"}}>
-                                <Link to="/get-involved" className={styles.navTextNoHover}>Get Involved</Link>
-                            </NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link>
-                            <Link to="/press" className={styles.navText}>Press</Link>
-                        </Nav.Link>
-                        <Nav.Link>
-                            <Link to="/play" className={styles.navText}>Play!</Link>
-                        </Nav.Link>
-                    </Nav>
-                </Nbar.Collapse>
-                <Nbar.Collapse className="justify-content-end">
-                    <Nav className="ms-4" navbarScroll>
-                        <Form className="d-flex">
-                            <Form.Control type="search" placeholder="Search" className="me-1" />
-                            <Button variant="outline-secondary" className="me-3 px-2"><img src={search_icon} width="23vh" height="23vh" /></Button>
-                        </Form>
-                        <Nav.Link className="me-3 px-auto">
-                            <Link to="/shop"><img src={cart_empty} width="20vh" height="20vh" /></Link>
-                        </Nav.Link>
-                    </Nav>
-                </Nbar.Collapse>
-            </Container> 
+        <Nbar sticky="top" variant="light" className={styles.nav + " navbar-light bg-light"} expand="lg">
+            <Nbar.Brand className="mx-4 px-0"> 
+                <Link to="/"><img src={logo} width="120vh" height="52vh" className="d-inline-block align-top" alt="Logo" /></Link>
+            </Nbar.Brand>
+            <Nbar.Toggle />
+            <Nbar.Collapse>
+                <Nav variant="primary" className="mx-3">
+                    <Link to="/" className={styles.navText + " nav-link text-primary"}>Home</Link>
+                    <Link to="/shop" className={styles.navText + " nav-link text-primary"}>Shop</Link>
+                    <NavDropdown title={learnElement} className={styles.navDropDown}>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/eco-friendly-brands" className={styles.navTextNoHover}>Eco-Friendly Brands</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/sustainability-activities" className={styles.navTextNoHover}>Sustainability Activities</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/blog" className={styles.navTextNoHover}>Our Blog</Link>
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title={impactElement} className={styles.navDropDown}>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/partners" className={styles.navTextNoHover}>Partners</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/philanthropy" className={styles.navTextNoHover}>Philanthropy</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/production" className={styles.navTextNoHover}>Production</Link>
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown title={aboutElement} className={styles.navDropDown}>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/meet-the-team" className={styles.navTextNoHover}>Meet The Team</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/contact-us" className={styles.navTextNoHover}>Contact Us</Link>
+                        </NavDropdown.Item>
+                        <NavDropdown.Item style={{backgroundColor: "white"}}>
+                            <Link to="/get-involved" className={styles.navTextNoHover}>Get Involved</Link>
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <Link to="/press" className={styles.navText + " nav-link text-primary"}>Press</Link>
+                    <Link to="/play" className={styles.navText + " nav-link text-primary"}>Play!</Link>
+                </Nav>
+            </Nbar.Collapse>
+            <Nbar.Collapse className="justify-content-end">
+                <Nav className="ms-4" navbarScroll>
+                    <Form className="d-flex">
+                        <Form.Control type="search" placeholder="Search" className="me-1" />
+                        <Button variant="outline-secondary" className="me-3 px-2"><img src={search_icon} width="23vh" height="23vh" /></Button>
+                    </Form>
+                    <Link to="/shop" className="nav-link me-3 px-auto"><img src={cart_empty} width="20vh" height="20vh" /></Link>
+                </Nav>
+            </Nbar.Collapse>
         </Nbar>
 
         <Outlet />
--- Website/Frontend/src/pages/Home.jsx
+++ Website/Frontend/src/pages/Home.jsx
@@ -1,19 +1,35 @@
 import { Button, Card } from 'react-bootstrap';
-import main_img from '../assets/main_img.png';
+import TitleBlock from '../components/TitleBlock.jsx'
+import idoriPlanet from '../assets/planet.png';
+import tractor from '../assets/tractor.png';
+import fullShip from '../assets/full_ship.png';
+import happyBenjy from '../assets/happy_benjy.png';
+import treehoppers from '../assets/treehoppers.png';
+import characters from '../assets/characters.png';
+import earth from '../assets/earth.png';
 import styles from './home.module.scss';
 
 function Home() {
     return (<>
-        <img className={styles.mainImg} src={main_img} width="100%" height="auto" alt="main image" />
-        <Card className={styles.imageCard + " text-primary bg-info"}>
-            <h2>Adopt an Idorian Today!</h2>
-            <p>These eco-friendly, adorable, huggable friends are on a mission to save 
-                the planet! Purchase one today and engage your kids with a new form of environmental 
-                learning!</p>
-            <Button className="text-white">
-                Shop Now!
-            </Button>
-        </Card>
+        <div className={styles.mainBodyDiv}>
+            <TitleBlock animation={false} left={<h1>Welcome to the Planet of Idori</h1>} right={<img src={idoriPlanet} height="80%" width="80%" className="mx-auto my-auto" />} txtColor="primary" />
+            <TitleBlock animation={true} left={<img src={treehoppers} height="80%" width="80%" />} right={<h2>The Idorians once lived happily together on their home planet Idori</h2>} txtColor="teal" />
+            <TitleBlock animation={true} left={<h2>But they didn't know that their behaviors were bad for their planet</h2>} right={<img src={tractor} height="55%" width="55%" />} txtColor="brown" />
+            <TitleBlock animation={true} left={<img src={fullShip} height="100%" width="100%" />} right={<h2>Because of this they had to leave to find a new home</h2>} txtColor="purple" />
+            <TitleBlock animation={true} left={<h2>Eventually, they found a beautiful planet called Earth</h2>} right={<img src={earth} height="80%" width="80%" />} txtColor="blue" />
+            <TitleBlock animation={true} left={<img src={happyBenjy} height="40%" width="40%" />} right={<h2>Now the Idorians teach the children of Earth about sustainability</h2>} txtColor="teal" />
+            <TitleBlock animation={true} left={<h2>So our planet remains beautiful for many generations to come</h2>} right={<img src={characters} height="80%" width="80%" />} txtColor="white" />
+
+            <Card className={styles.imageCard + " d-flex"}>
+                <div className={styles.placeholderScroll} />
+                <Card className={styles.innerCard + " mt-auto card-footer text-primary bg-info"}>
+                    <h2>Adopt an Idorian Today!</h2>
+                    <Button className="justify-content-center text-white">
+                        Shop Now!
+                    </Button>
+                </Card>
+            </Card>
+        </div>
     </>);
 }
 
