dd3abe833e4df73a6ad967d21a895e47dcbfc36d
Neet <75447256+Chhez@users.noreply.github.com>
2024-08-16 21:07:40 -0700

    Created Meet The Team Page

--- Website/Frontend/src/pages/MeetTheTeam.jsx
+++ Website/Frontend/src/pages/MeetTheTeam.jsx
@@ -1,4 +1,7 @@
 import styles from './meettheteam.module.scss';
+import SingleBlock from '../components/SingleBlock.jsx';
+import TripleBlock from '../components/TripleBlock.jsx';
+import { Card } from 'react-bootstrap';
 
 const teamTitle = "Meet the Dedicated Team Behind Idori | Get to Know Our Passionate Experts";
 
@@ -6,7 +9,102 @@ function MeetTheTeam() {
     document.title = teamTitle;
 
     return (<>
-        <h1>Meet The Team</h1>
+        <SingleBlock mt="10" mb="0" content={<>
+            <h1 className="text-start text-primary">Meet Our Amazing Team!</h1>
+        </>} />
+        <TripleBlock mt="10" mb="0" left={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2024-07-02_at_2.15.04_AM.png?v=1719900913" />
+                <Card.Body>
+                    <Card.Title>Noah Sorin <br />Founder/CEO</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        Noah is an early childhood educator, a children's book author, a social entrepreneur, an activist, and an avid environmentalist. After growing up on a farm in Montgomery, VT, and then moving to Boston, Noah became determined to spread his love for nature and sustainability to children all around the world using the magic of storytelling!
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} middle={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2022-06-07_at_1.54.49_AM.png?v=1654581298" />
+                <Card.Body>
+                    <Card.Title>Shumita Littlefield - Head of Creative Design/Illustration</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        Shumita has always been drawn to children’s books, toys, art, and other media. Conceptualizing and illustrating fictional characters and creatures has been an enduring hobby for her. Through her illustrations she hopes to fascinate and inspire children, and excite imaginations.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} right={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2023-06-30_at_7.38.26_PM.png?v=1688168331" />
+                <Card.Body>
+                    <Card.Title>Alia Bringas <br />Toy Designer</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        Aliaa is an Industrial design student at RISD focused primarily around sustainability and human centered design. Born and raised in Vermont, with a passion for the outdoors, she is always striving to make the planet a healthier and happier place in a fun and interesting way!
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} />
+        <TripleBlock mt="10" mb="0" left={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2023-07-01_at_4.09.34_PM.png?v=1688242311&width=710" />
+                <Card.Body>
+                    <Card.Title>Mackenzie Farley - Social Media Coordinator</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        As someone who hopes for a sustainable future, Mackenzie believes that teaching children about the environment is crucial for creating a future generation that understands and values our planet. From her experience, she has found entertainment to be an engaging medium for children.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} middle={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2024-07-02_at_2.07.21_AM.png?v=1719900474&width=710" />
+                <Card.Body>
+                    <Card.Title>Levy Te - Business Development Coordinator</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        Levy Te grew up in Lowell, MA, and is the daughter of Cambodian immigrants. She has an MBA and is a scientist by training. She is dedicated to making a positive impact on her community by educating the younger generation through fun and engaging activities like community service, interactive workshops, and mentorship.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} right={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2024-07-03_at_11.17.00_AM.png?v=1720019834&width=710" />
+                <Card.Body>
+                    <Card.Title>Bobby Chen - Business Development Intern</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        Bobby is deeply committed to giving back to the communities he grew up in. With a strong focus on early childhood education, he aims to provide opportunities for all children to be successful, healthy, and thoughtful. 
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} />
+        <TripleBlock mt="10" mb="10" left={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2023-07-01_at_4.13.10_PM.png?v=1688242397" />
+                <Card.Body>
+                    <Card.Title>Wayne Wang - Game Designer and Developer</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        Wayne has been concerned about the harmful effects of climate change since he witnessed a forest fire decimate a small town in British Colombia called Lytton. He was surprised by the lack of urgency he saw to fix this problem, so he's now devoted to spreading awareness on the importance of sustainability.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} middle={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2024-07-08_at_3.54.50_PM.png?v=1720468575&width=710" />
+                <Card.Body>
+                    <Card.Title>Jasmine Gohil - Business Development Intern</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                        Jasmine Gohil, a Master of Science candidate in Business Analytics with a passion for early childhood education. She previously served as a mentor, fostering the social and emotional development of a child in need. This experience, coupled with her interest in the environment, fuels her desire to cultivate a generation of responsible and environmentally conscious young minds.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} right={<>
+            <Card className="text-primary">
+                <Card.Img variant="top" height="400svh" src="https://idori.com/cdn/shop/files/Screen_Shot_2024-07-08_at_11.51.16_AM.png?v=1720453894&width=710" />
+                <Card.Body>
+                    <Card.Title>Myles St. Jean - Software Engineer</Card.Title>
+                    <Card.Text style={{height: "45svh"}}>
+                    Myles's enthusiasm for environmental sustainability stems from a strong belief in our duty to make the world a better place today and forever. He is committed to sparking children's interest in the environment through fun and interactive education, viewing it as essential for building a sustainable tomorrow.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </>} />
     </>);
 }
 
--- Website/Frontend/src/pages/Press.jsx
+++ Website/Frontend/src/pages/Press.jsx
@@ -1,4 +1,7 @@
 import styles from './press.module.scss';
+import SingleBlock from '../components/SingleBlock.jsx';
+import DoubleBlock from '../components/DoubleBlock.jsx';
+import { Image, Button } from 'react-bootstrap';
 
 const pressTitle = "Idori's Performance: Celebrating Successes and Awards in Sustainability and Education";
 
@@ -6,7 +9,52 @@ function Press() {
     document.title = pressTitle;
 
     return (<>
-        <h1>Press</h1>
+        <SingleBlock mt="10" mb="0" content={<>
+            <h1 className="text-primary">Celebrating Early Successes and <br />Awards in Sustainable Education</h1>
+            <p className="text-primary">As a testament to our dedication to sustainability education, we have received prestigious awards and <br />accolades for our innovative approach in fostering environmental awareness among children.</p>
+        </>} />
+        <DoubleBlock mt="10" mb="0" left={<>
+            <Image src="https://idori.com/cdn/shop/files/Screen_Shot_2024-06-30_at_5.24.12_PM.png?v=1719782667" fluid />
+        </>} right={<>
+            <h1 className="text-primary">Collegepreneur <br />Podcast</h1>
+            <p className="text-primary">Philip Eviston interviews Idori Founder and CEO, Noah<br /> Sorin, on the Collegepreneur Podcast to learn about his<br /> journey as a student entrepreneur.</p>
+            <Button className="text-light" variant="primary">Watch Now</Button>
+        </>} />
+        <DoubleBlock mt="10" mb="0" left={<>
+            <Image src="https://idori.com/cdn/shop/files/Screen_Shot_2024-06-30_at_5.06.00_PM.png?v=1719781697&width=750" fluid />
+        </>} right={<>
+            <h1 className="text-primary">Canvas Rebel <br />Feature</h1>
+            <p className="text-primary">Canvas Rebel interviews Idori Founder and CEO, Noah Sorin, in order to learn more about the backstory of Idori, how it got started, and where he hopes to take it.</p>
+            <Button className="text-light" variant="primary">Read More</Button>
+        </>} />
+        <DoubleBlock mt="10" mb="0" left={<>
+            <Image src="https://idori.com/cdn/shop/files/Screen_Shot_2023-09-11_at_4.20.17_PM.png?v=1694463625&width=750" fluid />
+        </>} right={<>
+            <h1 className="text-primary">BU Today Article</h1>
+            <p className="text-primary">BU Today features Idori and talks about founder Noah Sorin's journey through this entrepreneurial experience. Click the button below to read more about Noah's journey and how he got to where he is today.</p>
+            <Button className="text-light" variant="primary">Read More</Button>
+        </>} />
+        <DoubleBlock mt="10" mb="0" left={<>
+            <Image src="https://idori.com/cdn/shop/files/PoetsandQuants-for-Undergrads-MostDisruptiveStartups-2022-13-Collage-copy.jpg?v=1688654688&width=750" fluid />
+        </>} right={<>
+            <h1 className="text-primary">Poets and Quants Most Disruptive <br />Undergraduate Startups of 2022</h1>
+            <p className="text-primary">Idori named one of Poets and Quants' Most Disruptive Undergraduate Startups of 2022, recognizing their innovative approach to sustainability education.</p>
+            <Button className="text-light" variant="primary">Read More</Button>
+        </>} />
+        <DoubleBlock mt="10" mb="0" left={<>
+            <Image src="https://idori.com/cdn/shop/files/Scb-x48s_400x400_d601ae15-cdc1-4db3-9ff9-b5abbb352418.jpg?v=1688654726" fluid />
+        </>} right={<>
+            <h1 className="text-primary">Daily Free Press</h1>
+            <p className="text-primary">Idori featured in the Daily Free Press, highlighting their pioneering efforts in sustainability education and their significant impact on young learners.</p>
+            <Button className="text-light" variant="primary">Read More</Button>
+        </>} />
+        <DoubleBlock mt="10" mb="10" left={<>
+            <Image src="https://idori.com/cdn/shop/files/websitemetatag2022.png?v=1688654795&width=750" fluid />
+        </>} right={<>
+            <h1 className="text-primary">Mass Challenge</h1>
+            <p className="text-primary">Idori selected for MassChallenge 2023 Early Stage Growth Cohort on the social impact track, recognizing their exceptional potential and commitment to making a positive difference through sustainability education.</p>
+            <Button className="text-light" variant="primary">Read More</Button>
+        </>} />
     </>);
 }
 
