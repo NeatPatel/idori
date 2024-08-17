38211cea6a2c75b7b4428b1d29e591ebe70a383f
Neet <75447256+Chhez@users.noreply.github.com>
2024-08-16 20:36:10 -0700

    Added Blog Skeleton

--- Website/Frontend/src/pages/Blog.jsx
+++ Website/Frontend/src/pages/Blog.jsx
@@ -11,22 +11,11 @@ function Blog() {
     // Here is an example implementation of each component!
     // NOTE: DO NOT FORGET TO IMPORT THE COMPONENTS AT THE TOP!
     return (<>
-        <SingleBlock mt="50" mb="10" content={<>
+        <SingleBlock mt="10" mb="0" content={<>
             <h1 className="text-primary">Hello There! This is an example</h1>
         </>} />
-
-        <DoubleBlock mt="10" mb="0" left={<>
-            <h1 className="text-primary">Left Side</h1>
-        </>} right={<>
-            <h1 className="text-primary">Right Side</h1>
-        </>} />
-
-        <TripleBlock mt="25" mb="5" left={<>
-            <h1 className="text-primary">Left Side</h1>
-        </>} middle={<>
-            <h1 className="text-primary">Middle</h1>
-        </>} right={<>
-            <h1 className="text-primary">Right Side</h1>
+        <SingleBlock mt="10" mb="10" content={<>
+            <h1 className="text-primary">Blog API Content here</h1>
         </>} />
     </>);
 }
